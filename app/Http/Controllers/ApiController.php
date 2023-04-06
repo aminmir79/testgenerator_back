4d27b76dcd984e5a87785a7a9ea034ab4e56474d
aminmir79 <aminmax983@gmail.com>
2023-04-06 19:51:04 +0430

    create pdf in get quiz

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,7 +9,7 @@ use App\Models\Order;
 use App\Models\Comment;
 use App\Models\Question;
 use Auth;
-
+use PDF;
 
 class ApiController extends Controller
 {
@@ -49,7 +49,100 @@ class ApiController extends Controller
 
    public function get_Quiz(Request $request)
    {
-    return Order::find($request->id)->questions;
+    $order=Order::with('topic','questions')->find($request->id);
+      PDF::SetTitle('quiz');
+      PDF::AddPage();
+      // set some language dependent data:
+      $lg = Array();
+      $lg['a_meta_charset'] = 'UTF-8';
+      $lg['a_meta_dir'] = 'rtl';
+      $lg['a_meta_language'] = 'fa';
+      $lg['w_page'] = 'page';
+
+      // set some language-dependent strings (optional)
+      PDF::setLanguageArray($lg);
+      PDF::SetFont('xkoodak', '', 18);
+      $file='<!DOCTYPE html>
+      <html lang="en" dir="rtl">
+        <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="stylesheet" href="./style.css/style.css" />
+          <title>Document</title>
+        </head>
+        <body>
+          <div class="container">
+            <header class="header">
+              <ul class="d-flex jus-flex infoList">
+                <li>نام:</li>
+                <li>نام خانوادگی :</li>
+                <li>موضوع :'.$order->topic->name.'</li>
+                <li>زمان:'.$order->time.'</li>
+              </ul>
+            </header>
+            <div>
+              <ul class="d-flex jus-flex quizList">';
+              
+              foreach ($order->questions as $q){
+                $file=$file.'<li>'.$q->question.' (1نمره)</li>';
+              }
+              $file=$file.'</ul></div></div></body></html>';
+
+
+      $file=$file.'<Style>* {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+      ul,
+      li {
+        list-style-type: none;
+      }
+      .container {
+        margin: 40px;
+        padding: 25px;
+        border: 2px solid;
+        border-radius: 10px;
+        height: 1000px;
+        background-color: #f1f3f5;
+      }
+      .d-flex {
+        display: flex;
+        align-items: center;
+      }
+      
+      .jus-flex {
+        flex-direction: row;
+        justify-content: space-evenly;
+      }
+      .header {
+        height: 100px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 3px solid;
+        border-radius: 10px;
+        font-size: 18px;
+        font-weight: 600;
+        margin-bottom: 60px;
+      }
+      
+      .infoList {
+        width: 100%;
+      }
+      
+      .quizList {
+        padding: 10px;
+        flex-direction: column;
+        align-items: start;
+        gap: 50px;
+        font-size: 20px;
+      }
+      </Style>';
+      PDF::writeHTML($file);
+      
+      return PDF::Output('quiz.pdf');
    }
 
    public function order(Request $request)
